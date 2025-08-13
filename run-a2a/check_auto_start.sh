e813ba318fe877f35703a35ece39bff13fade73a
Diego Fornalha <diegofornalha@gmail.com>
2025-08-12 21:32:27 -0300

    novo

--- run-a2a/check_auto_start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Script para verificar se auto-start está desabilitado
-
-if [ -f "auto_start_disabled.flag" ]; then
-    echo "🚫 Auto-start está desabilitado. Para reabilitar, execute: chmod +x ../agent_manager.sh"
-    exit 1
-fi
